aaaf3e01a1ab5b2e00e7ff90336fea4316a12dc1
VanceNykreim <vanceyadi@msn.com>
2023-10-24 15:50:05 -0600

    more work done on Unit 04

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -3,3 +3,15 @@ const date = new Date();
 document.querySelector("#year").textContent = date.getFullYear();
 
 document.querySelector("#lastmodified").textContent = document.lastModified;
+
+
+
+// for hamburger menu
+var checkbox = document.getElementById("hamburger");
+    var nav = document.getElementById("hamnav");
+
+    // Add an event listener to the checkbox
+    checkbox.addEventListener("change", function() {
+      // Toggle the active class on the nav element
+      nav.classList.toggle("active");
+    });
