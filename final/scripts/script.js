86b0c23a470d014648a2486acd4217a42904ca7b
VanceNykreim <vanceyadi@msn.com>
2023-12-05 18:25:27 -0700

    worked on header nav

--- final/scripts/script.js
+++ final/scripts/script.js
@@ -1,10 +1,12 @@
-// Hamburger menu functionality
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const nav = document.querySelector('nav');
+// for hamburger menu
+var checkbox = document.getElementById("hamburger");
+    var nav = document.getElementById("hamnav");
 
-hamburgerMenu.addEventListener('click', () => {
-  nav.classList.toggle('show');
-});
+    // Add an event listener to the checkbox
+    checkbox.addEventListener("change", function() {
+      // Toggle the active class on the nav element
+      nav.classList.toggle("active");
+    });
 
 // Last modified date in the footer
 const lastModified = document.querySelector('.last-modified');
